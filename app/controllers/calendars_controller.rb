1a2ff90499e93a6acd7380858bafd9a033dbc79f
Harry Boyes <harry.boyes@gmail.com>
2016-11-14 16:33:05 +0000

    Group calendar list by account profile

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -1,6 +1,6 @@
 class CalendarsController < ApplicationController
   def index
-    @calendars = cronofy.list_calendars
+    @account_profiles = cronofy.list_calendars.group_by { |calendar| { profile_id: calendar.profile_id, profile_name: calendar.profile_name } }
   end
 
   def show
