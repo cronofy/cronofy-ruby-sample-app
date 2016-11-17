bdb87e29044552a977296c9c69b36df45ba608f6
Harry Boyes <harry.boyes@gmail.com>
2016-11-17 10:25:16 +0000

    Implement 'no credentials' page

--- app/controllers/root_controller.rb
+++ app/controllers/root_controller.rb
@@ -2,5 +2,12 @@ class RootController < ApplicationController
   skip_before_action :authorize
 
   def show
+    unless ENV['CRONOFY_CLIENT_ID'] and ENV['CRONOFY_CLIENT_SECRET']
+      render :no_credentials and return
+    end
+
+    unless logged_in?
+      render :login and return
+    end
   end
 end
