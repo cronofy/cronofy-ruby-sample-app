b6e626dad78bce04b9286b8844e71892d3b28a6f
Harry Boyes <harry.boyes@gmail.com>
2016-11-23 12:38:35 +0000

    Add logging around each cronofy_client method

--- app/models/cronofy_client.rb
+++ app/models/cronofy_client.rb
@@ -3,63 +3,99 @@ class CronofyClient
   end
 
   def initialize(user)
+    Rails.logger.info { "Cronofy client initialized - user.id=#{user.id} - access_token=#{user.cronofy_access_token} - refresh_token=#{user.cronofy_refresh_token}" }
     @user = user
   end
 
   def account
-    cronofy_request { cronofy.list_profiles }
+    Rails.logger.info { "Cronofy request - account" }
+    response = cronofy_request { cronofy.list_profiles }
+    Rails.logger.info { "Cronofy response - account - #{response.inspect}" }
+    response
   end
 
   def list_calendars
-    cronofy_request { cronofy.list_calendars }
-  end
-
-  def read_all_events
-    cronofy_request { cronofy.read_events(include_managed: true) }
+    Rails.logger.info { "Cronofy request - list_calendars" }
+    response = cronofy_request { cronofy.list_calendars }
+    Rails.logger.info { "Cronofy response - list_calendars - #{response.inspect}" }
+    response
   end
 
   def read_events(calendar_id)
-    cronofy_request { cronofy.read_events(include_managed: true, calendar_ids: [calendar_id]) }
+    Rails.logger.info { "Cronofy request - read_events - calendar_id=#{calendar_id}" }
+    response = cronofy_request { cronofy.read_events(include_managed: true, calendar_ids: [calendar_id]) }
+    Rails.logger.info { "Cronofy response - read_events - #{response.inspect}" }
+    response
   end
 
   def upsert_event(event)
-    cronofy_request { cronofy.upsert_event(event.calendar_id, event.data) }
+    Rails.logger.info { "Cronofy request - upsert_event - event=#{event.inspect}" }
+    response = cronofy_request { cronofy.upsert_event(event.calendar_id, event.data) }
+    Rails.logger.info { "Cronofy response - upsert_event - #{response.inspect}" }
+    response
   end
 
   def list_profiles
-    cronofy_request { cronofy.list_profiles }
+    Rails.logger.info { "Cronofy request - list_profiles" }
+    response = cronofy_request { cronofy.list_profiles }
+    Rails.logger.info { "Cronofy response - list_profiles - #{response.inspect}" }
+    response
   end
 
   def create_calendar(calendar)
-    cronofy_request { cronofy.create_calendar(calendar.profile_id, calendar.name) }
+    Rails.logger.info { "Cronofy request - create_calendar - name=#{calendar.name} - profile_id=#{calendar.profile_id}" }
+    response = cronofy_request { cronofy.create_calendar(calendar.profile_id, calendar.name) }
+    Rails.logger.info { "Cronofy response - create_calendar - #{response.inspect}" }
+    response
   end
 
   def delete_event(calendar_id, event_uid)
-    cronofy_request { cronofy.delete_event(calendar_id, event_uid) }
+    Rails.logger.info { "Cronofy request - delete_event - calendar_id=#{calendar_id} - event_uid=#{event_uid}" }
+    response = cronofy_request { cronofy.delete_event(calendar_id, event_uid) }
+    Rails.logger.info { "Cronofy response - delete_event - #{response.inspect}" }
+    response
   end
 
   def list_channels
-    cronofy_request { cronofy.list_channels }
+    Rails.logger.info { "Cronofy request - list_channels" }
+    response = cronofy_request { cronofy.list_channels }
+    Rails.logger.info { "Cronofy response - list_channels - #{response.inspect}" }
+    response
   end
 
   def create_channel(channel)
-    cronofy_request { cronofy.create_channel(ENV['DOMAIN'] + "/push/#{channel.path}", filters: { only_managed: channel.only_managed, calendar_ids: channel.calendar_ids }) }
+    Rails.logger.info { "Cronofy request - create_channel - url=#{ENV['DOMAIN'] + "/push/#{channel.path}" } - only_managed=#{channel.only_managed} - calendar_ids=#{channel.calendar_ids}" }
+    response = cronofy_request { cronofy.create_channel(ENV['DOMAIN'] + "/push/#{channel.path}", filters: { only_managed: channel.only_managed, calendar_ids: channel.calendar_ids }) }
+    Rails.logger.info { "Cronofy response - create_channel - #{response.inspect}" }
+    response
   end
 
   def close_channel(channel_id)
-    cronofy_request { cronofy.close_channel(channel_id) }
+    Rails.logger.info { "Cronofy request - close_channel - channel_id=#{channel_id}" }
+    response = cronofy_request { cronofy.close_channel(channel_id) }
+    Rails.logger.info { "Cronofy response - close_channel - #{response.inspect}" }
+    response
   end
 
   def free_busy
-    cronofy_request { cronofy.free_busy }
+    Rails.logger.info { "Cronofy request - free_busy" }
+    response = cronofy_request { cronofy.free_busy }
+    Rails.logger.info { "Cronofy response - free_busy - #{response.inspect}" }
+    response
   end
 
   def resources
-    cronofy_request { cronofy.resources }
+    Rails.logger.info { "Cronofy request - resources" }
+    response = cronofy_request { cronofy.resources }
+    Rails.logger.info { "Cronofy response - resources - #{response.inspect}" }
+    response
   end
 
   def authorize_with_service_account(user, scopes, callback_url)
-    cronofy_request { cronofy.authorize_with_service_account(user.email, scopes, callback_url) }
+    Rails.logger.info { "Cronofy request - authorize_with_service_account - user.id=#{user.id} - scopes=#{scopes} - callback_url=#{callback_url}"}
+    response = cronofy_request { cronofy.authorize_with_service_account(user.email, scopes, callback_url) }
+    Rails.logger.info { "Cronofy response - authorize_with_service_account - #{response.inspect}" }
+    response
   end
 
   private
