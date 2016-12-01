3561770edcbe8d0a09a2a2d9c1b4ed373ce22f30
Harry Boyes <harry.boyes@gmail.com>
2016-12-01 11:17:04 +0000

    Include local_env.yml environment variables

--- config/application.rb
+++ config/application.rb
@@ -13,3 +13,10 @@ module CronofySampleApp
     # -- all .rb files in that directory are automatically loaded.
   end
 end
+
+Rails.application.config.before_configuration do
+  env_file = File.join(Rails.root, 'config', 'local_env.yml')
+  YAML.load(File.open(env_file)).each do |key, value|
+    ENV[key.to_s] = value
+  end if File.exists?(env_file)
+end
